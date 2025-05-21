500455bfd5a78b59ec8c4409ebe5c972e2b6c6ed
Sangwook02 <chosw1002@naver.com>
2025-05-21 10:45:05 +0900

    feat: 누락된 파일들 추가

---
+++ src/main/java/com/example/demo/YoonsweApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoonsweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoonsweApplication.class, args);
+	}
+
+}
